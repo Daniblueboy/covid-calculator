29fa245df75483206e6b9f5cf7d046a3d861ba30
Daniel Obasuyi <danielobasuyi@gmail.com>
2022-07-14 21:46:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import Home from './components/Home';
+import './App.css';
+
+export class App extends Component {
+  render() {
+    return <Home />;
+  }
+}
+
+export default App;
